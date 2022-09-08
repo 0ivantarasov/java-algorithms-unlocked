e69c7bb3cfcfdd46c5828a9e7202c01301165a2d
0ivantarasov <vvv3ww2@vk.com>
2022-09-09 00:50:42 +0700

    Initial commit

---
+++ src/main/java/com/java/algorithms/unlocked/App.java
@@ -0,0 +1,13 @@
+package com.java.algorithms.unlocked;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
